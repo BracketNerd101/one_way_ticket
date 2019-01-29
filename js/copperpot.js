ba003ece54f4e1f2e467b6ce57ad37137c4790a6
Arthur C Edwards <Arthur.Edwards321@gmail.com>
2019-01-28 21:16:27 -0500

    motion is set up. with new password

--- js/copperpot.js
+++ js/copperpot.js
@@ -5,8 +5,30 @@ while (testV < 3) {
 if (!pass1)
 history.go(-1);
 if (pass1.toLowerCase() == "greenbutterfly") {
-alert('You Got it Right!');
+alert('Correct Click ok to see portfolio!');
 window.open('fb_billing.html');
+break;
+}
+testV+=1;
+var pass1 =
+prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+}
+if (pass1.toLowerCase()!="password" & testV ==3)
+history.go(-1);
+return " ";
+}
+
+// THis is for the different passWord
+
+function passWord2() {
+var testV = 1;
+var pass1 = prompt('Portfolio is Private, Enter password',' ');
+while (testV < 3) {
+if (!pass1)
+history.go(-1);
+if (pass1.toLowerCase() == "fiddle") {
+alert('Correct Click ok to see portfolio!');
+window.open('motion.html');
 break;
 }
 testV+=1;
